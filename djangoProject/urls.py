2eaa126de02e3bae0c468502e312ecb8680d26c3
IvanJrJr <ivanoloko1000@gmail.com>
2021-02-02 02:15:00 -0300

    Configurando arquivo de rotas
    Configurando arquivo de rotas

--- djangoProject/urls.py
+++ djangoProject/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from main.views import contato, index
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', index),
+    path('contato', contato),
 ]
