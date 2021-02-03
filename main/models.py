ffe1c5a6164717ed5546d04cda3b7bec3dcec09a
IvanSJr <ivanjrjesus01@gmail.com>
2021-02-02 23:18:58 -0300

    Criando modelo de Produtos. Com Nome, Preço,

--- main/models.py
+++ main/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+
+class Produto(models.Model):
+    nome = models.CharField('Nome', max_length=100)
+    preco = models.DecimalField('Preço', max_digits=10, decimal_places=2)
+    estoque = models.IntegerField('Quantidade de Estoque')
