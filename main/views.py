1f5a055b1ffb171ddb3741fffb5d9c3641cbc885
IvanSJr <ivanjrjesus01@gmail.com>
2021-02-03 13:04:56 -0300

    Fazendo crud de Produtos do banco de dados para página index.html

--- main/views.py
+++ main/views.py
@@ -1,14 +1,18 @@
 from django.shortcuts import render
+from main.models import Cliente, Produto
 
 
 def index(request):
+    produto = Produto.objects.all()
+
     if str(request.user) == 'AnonymousUser':
         usuario = 'Usuário Anônimo'
     else:
         usuario = f'{request.user.first_name} logado'
     context = {
         'curso': 'Programação web com Django-Framework',
-        'verifica_logado': usuario
+        'verifica_logado': usuario,
+        'produtos': produto
     }
     return render(request, 'index.html', context)
 
