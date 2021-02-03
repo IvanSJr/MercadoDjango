cd0e875b99a8146e811caabcba91613fb1afe2d1
IvanSJr <ivanjrjesus01@gmail.com>
2021-02-03 11:37:01 -0300

    Fazendo um list_display para mostrar dados no painel administrativo do Django.

--- main/admin.py
+++ main/admin.py
@@ -2,5 +2,14 @@ from django.contrib import admin
 
 from .models import Produto, Cliente
 
-admin.site.register(Produto)
-admin.site.register(Cliente)
+
+class ProdutoAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'preco', 'estoque')
+
+
+class ClienteAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'sobrenome', 'email')
+
+
+admin.site.register(Produto, ProdutoAdmin)
+admin.site.register(Cliente, ClienteAdmin)
