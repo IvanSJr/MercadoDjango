ad18d1f4aa26a40a74275a8e7083c50b1426c36f
IvanSJr <ivanjrjesus01@gmail.com>
2021-02-02 23:28:45 -0300

    Criando modelo Cliente e aplicando com o migrate

--- main/models.py
+++ main/models.py
@@ -5,3 +5,9 @@ class Produto(models.Model):
     nome = models.CharField('Nome', max_length=100)
     preco = models.DecimalField('Preço', max_digits=10, decimal_places=2)
     estoque = models.IntegerField('Quantidade de Estoque')
+
+
+class Cliente(models.Model):
+    nome = models.CharField('Nome', max_length=100)
+    sobrenome = models.CharField('Sobrenome', max_length=100)
+    email = models.EmailField('E-mail', max_length=100)
