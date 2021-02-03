d6c4d7b372124599ce059a2d65bab09193411b4c
IvanSJr <ivanjrjesus01@gmail.com>
2021-02-03 12:43:40 -0300

    Implementando funcionalidade que verifica se o usuário está logado ou não. Se o usuário estiver logado retorna o seu primeiro nome.

--- main/views.py
+++ main/views.py
@@ -2,8 +2,13 @@ from django.shortcuts import render
 
 
 def index(request):
+    if str(request.user) == 'AnonymousUser':
+        usuario = 'Usuário Anônimo'
+    else:
+        usuario = f'{request.user.first_name} logado'
     context = {
-        'curso': 'Programação web com Django-Framework'
+        'curso': 'Programação web com Django-Framework',
+        'verifica_logado': usuario
     }
     return render(request, 'index.html', context)
 
