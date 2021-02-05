c2beea96652931108a2727f25c5aa09656653b3f
IvanSJr <ivanjrjesus01@gmail.com>
2021-02-05 00:19:17 -0300

    Configurando para publicação

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 't5)sk@^brfpbwr$4-15t!2z8v5m20+)ue3i=4u5_aivc&tl8w^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
