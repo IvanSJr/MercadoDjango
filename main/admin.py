449ce3422be0c5f81ae22b7c068c4d0bfaaab393
IvanSJr <ivanjrjesus01@gmail.com>
2021-02-06 10:29:35 -0300

    Definindo e configurando os models de Cliente e Produto

--- main/admin.py
+++ main/admin.py
@@ -3,13 +3,13 @@ from django.contrib import admin
 from .models import Produto, Cliente
 
 
+@admin.register(Produto)
 class ProdutoAdmin(admin.ModelAdmin):
-    list_display = ('nome', 'preco', 'estoque')
+    list_display = ('nome', 'preco', 'estoque', 'slug', 'criado', 'modificado', 'ativo')
 
 
+@admin.register(Cliente)
 class ClienteAdmin(admin.ModelAdmin):
-    list_display = ('nome', 'sobrenome', 'email')
+    list_display = ('nome', 'sobrenome', 'email', 'criado', 'modificado', 'ativo')
 
 
-admin.site.register(Produto, ProdutoAdmin)
-admin.site.register(Cliente, ClienteAdmin)
--- main/models.py
+++ main/models.py
@@ -1,19 +1,45 @@
 from django.db import models
+from django.db.models import signals
+from django.template.defaultfilters import slugify
+
+class Base(models.Model):
+    criado = models.DateTimeField('Data de Criação', auto_now_add=True)
+    modificado = models.DateTimeField('Data de Atualização', auto_now=True)
+    ativo = models.BooleanField('Ativado?', default=True)
+
+    class Meta:
+        abstract = True
 
 
-class Produto(models.Model):
+class Produto(Base):
     nome = models.CharField('Nome', max_length=100)
     preco = models.DecimalField('Preço', max_digits=10, decimal_places=2)
     estoque = models.IntegerField('Quantidade de Estoque')
+    slug = models.SlugField('Slug', max_length=100, editable=False, blank=True)
 
     def __str__(self):
         return f'{self.nome} {self.estoque}'
 
 
-class Cliente(models.Model):
+def produto_pre_save(signal, instance, sender, **kwargs):
+    instance.slug = slugify(instance.nome)
+
+
+signals.pre_save.connect(produto_pre_save, sender=Produto)
+
+
+class Cliente(Base):
     nome = models.CharField('Nome', max_length=100)
     sobrenome = models.CharField('Sobrenome', max_length=100)
     email = models.EmailField('E-mail', max_length=100)
+    slug = models.SlugField('Slug', max_length=100, editable=False, blank=True)
 
     def __str__(self):
         return f'{self.nome.title()} {self.sobrenome.title()}'
+
+
+def cliente_pre_save(signal, instance, sender, **kwargs):
+    instance.slug = slugify(instance.nome)
+
+
+signals.pre_save.connect(cliente_pre_save, sender=Cliente)
